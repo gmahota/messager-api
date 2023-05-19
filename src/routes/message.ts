2b2e454d3411f5515da211ae6a259058aacc83f0
Guimaraes.Mahota <guimaraesmahota@gmail.com>
2023-05-19 21:18:44 +0200

    prepare to deploy

--- src/routes/message.ts
+++ src/routes/message.ts
@@ -9,24 +9,48 @@ export async function messageRoutes(fastify: FastifyInstance) {
         return { count }
     })
 
-    fastify.post('/messages/create', async (request, repl) => {
+    fastify.post('/messages/create', async (request, replay) => {
 
         const createMessageBody = z.object({
             name: z.string(),
             cellphone: z.string(),
             text: z.string(),
             message: z.string(),
-            replay:z.string()
+            reply:z.string()
         })
 
-        const { name, cellphone, text, message,replay } = createMessageBody.parse(request.body)
+        const { name, cellphone, text, message,reply } = createMessageBody.parse(request.body)
 
         const _message = await prisma.message.create({
             data: {
-                name, cellphone, text, message,replay
+                name, cellphone, text, message,reply
             }
         })
 
         return { _message }
+    })
+
+    fastify.get('/messages/:cellphone/:from', async (request) => {
+
+        const getMessageBody = z.object({
+            cellphone: z.string(),
+            from: z.string()
+        })
+
+        const { cellphone,from } = getMessageBody.parse(request.params)
+
+        const messages = await prisma.message.findMany({
+            where: {
+                cellphone: cellphone,
+                text: from,
+            },
+            orderBy: {
+                id: 'asc'
+            }
+        })
+
+        return {
+            messages
+        }
     })
 }
